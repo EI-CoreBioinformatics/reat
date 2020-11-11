ebf062f483b0a9762acbb7fb64243b673c0d969f
ljyanesm <yanes.luis@gmail.com>
2020-11-11 16:30:21 +0000

    Renames singularity definition file
    Fleshing out of the build_image script

--- singularity/build_image.sh
+++ singularity/build_image.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
-
+set -euxo
+version=0.0.1
+rundir=${PWD}
 cd $(mktemp -d)
-sudo singularity build reat.img REAT-Singularity.def
+sudo singularity build reat.img ${rundir}/reat_singularity.def
+mkdir -p /ei/software/testing/reat/${version}/x86_64/bin
+cp reat.img /ei/software/testing/reat/${version}/x86_64/reat-${version}.img
+cat > singularity.exec <<SINGULARITY_EXEC
+#!/bin/bash
+DIR=\`dirname \$(readlink -f \$0)\`
+singularity exec \$DIR/../reat-${version}.img \$(basename "\$0") \$@
+SINGULARITY_EXEC
