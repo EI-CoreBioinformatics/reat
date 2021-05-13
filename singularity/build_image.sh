4de447e218f58cc5106edb42a6453e01ee1e7f2b
ljyanesm <yanes.luis@gmail.com>
2021-05-13 18:57:00 +0100

    EI automate singularity script
    minor improv

--- singularity/build_image.sh
+++ singularity/build_image.sh
@@ -1,15 +1,15 @@
 set -euxo
 version=0.0.10
-rundir=$(dirname $0)
-cd $(mktemp -d)
-cp ${rundir}/reat_singularity.def reat.def
+rundir=$(dirname "$0")
+cd "$(mktemp -d)"
+cp "${rundir}"/reat_singularity.def reat.def
 sudo singularity build reat.img reat.def
 mkdir -p /ei/software/testing/reat/${version}/x86_64/bin
 cp reat.img /ei/software/testing/reat/${version}/x86_64/reat-${version}.img
 
 cat << EOF > /ei/software/testing/reat/${version}/x86_64/bin/singularity.exec
 #!/bin/bash
-DIR=`dirname $(readlink -f $0)`
+DIR=$(dirname "$(readlink -f "$0")")
 singularity exec $DIR/../reat-${version}.img $(basename "$0") $@
 EOF
 
