2788f81e9eb547b4cdd7a32be3c42a6ae6f413ce
ljyanesm <yanes.luis@gmail.com>
2021-05-31 00:18:32 +0100

    WiP - Github actions
    Fix the check for samples in grouped portcullis

--- annotation/transcriptome.py
+++ annotation/transcriptome.py
@@ -405,7 +405,7 @@ def sample_validation(cromwell_inputs):
             if len(paired_sample_names) == l:
                 raise ValueError(f"Sample {sample['name']} is repeated, please make sure sample names are unique")
 
-    if not sample_strand:
+    if cromwell_inputs.get("ei_annotation.wf_align.group_to_samples", None) and not sample_strand:
         raise ValueError(f"Sorry! It appears there are no paired samples to group. Did you mean to include the "
                          f"csv_paired_samples argument?")
     if cromwell_inputs.get("ei_annotation.wf_align.group_to_samples", None):
