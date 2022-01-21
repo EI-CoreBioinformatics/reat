adf4a021ab53e794426d0d7ebc5768b4bb070aea
ljyanesm <yanes.luis@gmail.com>
2022-01-21 12:30:15 +0000

    v0.6.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import glob
 
 setup(
     name='reat',
-    version='0.5.1',
+    version='0.6.0',
     packages=find_packages('.', exclude=["tests"]),
     url='https://github.com/ei-corebioinformatics/reat',
     classifiers=[
